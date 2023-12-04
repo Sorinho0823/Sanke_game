bcaef45a496c224cc89889084cc78d5e9b6551f1
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2023-12-04 13:45:16 +0100

    Works, playable, not interesting but playable

--- Snake_game/Form1.Designer.cs
+++ Snake_game/Form1.Designer.cs
@@ -29,20 +29,43 @@ namespace Snake_game
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PanelMap = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PanelMap
+            // 
+            this.PanelMap.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.PanelMap.Location = new System.Drawing.Point(33, 138);
+            this.PanelMap.Name = "PanelMap";
+            this.PanelMap.Size = new System.Drawing.Size(600, 300);
+            this.PanelMap.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PanelMap);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel PanelMap;
     }
 }
 
--- Snake_game/Form1.cs
+++ Snake_game/Form1.cs
@@ -12,9 +12,52 @@ namespace Snake_game
 {
     public partial class Form1 : Form
     {
+
+        int headX = 100;
+        int headY = 100;
+        int directionX = 1;
+        int directionY = 0;
+        int lenght = 5;
+        //int steps?
+
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            headX += directionX * SnakeBody.BodySize;
+            headY += directionY * SnakeBody.BodySize;
+
+            foreach (SnakeBody item in PanelMap.Controls)
+            {
+                if (item.Left == headX && item.Top == headY)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Game Over :(");
+                }
+
+            }
+            SnakeBody newHead = new SnakeBody();
+            newHead.Left = headX;
+            newHead.Top = headY;
+
+
+            PanelMap.Controls.Add(newHead);
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) // 
+        {
+            if (e.KeyCode == Keys.W) { directionX = 0; directionY = -1; } // up
+            if (e.KeyCode == Keys.S) { directionX = 0; directionY = 1; } // down
+            if (e.KeyCode == Keys.A) { directionX = -1; directionY = 0; } // left
+            if (e.KeyCode == Keys.D) { directionX = 1; directionY = 0; } // right
+
         }
 
 
